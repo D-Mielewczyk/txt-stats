d1a057a7841d9718f8ac9e749d67b5a537066a07
D-Mielewczyk <MDDawid0323@gmail.com>
2022-06-19 19:45:42 +0200

    secret key fix

--- txt_stats/txt_stats/settings.py
+++ txt_stats/txt_stats/settings.py
@@ -20,9 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-from decouple import config
+import os
 
-SECRET_KEY = config("SECRET_KEY") # this is to replace the secret key you cut away before
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
