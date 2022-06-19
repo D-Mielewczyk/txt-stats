e7c66faaca814da96824307cec708a730651b038
D-Mielewczyk <MDDawid0323@gmail.com>
2022-06-19 19:17:09 +0200

    add app, basic html

--- txt_stats/analyze_txt/views.py
+++ txt_stats/analyze_txt/views.py
@@ -1,3 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+def home(response):
+    return render(response, "analyze_txt/home.html", {})
